cb01e85a93038b494a703484adbcfad632d8b745
atom <solidajun@163.com>
2020-10-13 20:53:07 +0800

    P28 基于jQ的Ajax向后端发起DELETE请求

--- scripts/Mock.js
+++ scripts/Mock.js
@@ -1,6 +1,8 @@
 
+// Get an Order
 // Mock响应模板
-Mock.mock('http://test.com/api/orders', {
+//模拟get请求
+Mock.mock('http://test.com/api/orders', 'get', {
     "orderData": [{
         'id':1,
         'name':'James',
@@ -10,4 +12,22 @@ Mock.mock('http://test.com/api/orders', {
         'name':'John',
         'drink':'Milk'
     }]
+});
+
+// Add an Order
+//模拟post请求,外层的Mock只负责拦截接口和方法的定义
+Mock.mock('http://test.com/api/addOrders', 'post', function (option) {
+    //请求相关的参数
+    console.log(option);
+    var jsonOrder = JSON.parse(option.body);
+    //模拟假数据需要重新写Mock
+    return Mock.mock({
+        status:200,
+        message:'@cword(4,9)',
+        orderData: {
+            'id':3,
+            'name':jsonOrder.name,
+            'drink':jsonOrder.drink
+        }
+    });
 });
--- scripts/jq_ajax_tutorial.js
+++ scripts/jq_ajax_tutorial.js
@@ -1,17 +1,53 @@
 $(function () {
 
     var $orders = $('#orders');
+    var $name = $('#name');
+    var $drink = $('#drink');
+
+    function addOrder(order) {
+        $orders.append('<li>name: '+order.name + ', drink: ' + order.drink + '</li>');
+    }
+
     $.ajax({
         type:'GET',
         url: 'http://test.com/api/orders',
         dataType: 'json',
         success: function (orders) {
+            console.log(orders);
             var orderData = orders.orderData;
             $.each(orderData, function (i, order) {
-                $orders.append('<li>name: '+order.name + ', drink: ' + order.drink + '</li>');
+                addOrder(order);
             });
-            
+        },
+        error: function () {
+            alert('error loading orders');
         }
+    });
+
+    $('#add-order').on('click', function () {
+        
+        var order = {
+            name: $name.val(),
+            drink: $drink.val(),
+        };
+        var orderJson = JSON.stringify(order);
+        // var jsonOrder = $.toJSON(order);
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://test.com/api/addOrders',
+            // data: order,
+            data: orderJson,
+            contentType: 'application/json',
+            success: function (newOrder) {
+                var newOrderJobj = JSON.parse(newOrder);
+                addOrder(newOrderJobj.orderData);
+            },
+            error: function () {
+                alert('add order error loading orders');
+            }
+        });
+
     });
 
 });
