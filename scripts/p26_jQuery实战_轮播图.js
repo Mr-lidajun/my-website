494df6f8f325c6d3aa017e360e09376e0b1ad05e
atom <solidajun@163.com>
2020-10-13 16:00:54 +0800

    P25 jQuery实现轮播图

---
+++ scripts/p26_jQuery实战_轮播图.js
@@ -0,0 +1,36 @@
+// 'use strict';
+
+$(function () {
+    
+    //configuration
+    var width = 360;
+    var animationSpeed = 1000;
+    var pause = 1000;
+    var currentSlide = 1;
+
+    //cache DOM
+    var $slider = $('#slider');
+    var $slideContainer = $slider.find('.slides');
+    var $slides = $slideContainer.find('.slide');
+    console.log($slides);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(() => {
+            $slideContainer.animate({'margin-left':'-=' + width}, animationSpeed, function () {
+                currentSlide++;
+                if (currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);   
+    }
+
+    function stopSlider() {
+        clearInterval(interval);
+    }
+
+    $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+});
